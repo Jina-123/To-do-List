4ebb3e6508a8133906a0fedb6289ed8bcc5a6b94
Jinhee Yang <78382611+Jina-123@users.noreply.github.com>
2021-09-02 01:09:41 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import Form from './Components/Form'
+
+function App() {
+
+
+  return (
+    <div class="appli" className="App">
+      <h1 className="text-center mt-3">To-do List</h1>
+      <Form />
+    </div>
+    );
+  }
+
+export default App;
